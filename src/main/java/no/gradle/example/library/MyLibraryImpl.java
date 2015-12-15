db1bcf010cba80906480eaff7aa02b2a41eb6640
Magnus Skuland <magnus@jpro.no>
2015-12-15 22:38:27 +0100

    Added generated source dir under buildDir.

--- src/main/java/no/gradle/example/library/MyLibraryImpl.java
+++ src/main/java/no/gradle/example/library/MyLibraryImpl.java
@@ -1,12 +1,14 @@
 package no.gradle.example.library;
 
 import com.google.common.collect.Lists;
+import net.webservicex.GetWeather;
 
 import java.util.ArrayList;
 
 public class MyLibraryImpl{
 
     public int doSomeThingSmart() {
+        new GetWeather().getCityName();
         ArrayList<Object> objects = Lists.newArrayList();
         return objects.size();
     }
